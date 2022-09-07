077a0b10c3cff813806d0506696f87b9beb55e20
Adriano Castro <drineo@hotmail.com>
2022-09-07 18:39:15 -0300

    Alura_tracker_Adriano_v.1

---
+++ src/components/Box.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="box has-text-weight-bold">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'Box',
+
+})
+</script>
+
+<style scoped>
+.box {
+    background: #B4CEB3;
+    box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.6);
+}
+</style>
